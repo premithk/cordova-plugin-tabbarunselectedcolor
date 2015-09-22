1b190bd306c10dcf10b0ccf5d0537e5b7fbff336
premithk <premith@gmail.com>
2015-09-22 12:47:23 +0530

    Update tabbarunselected.js

--- www/tabbarunselected.js
+++ www/tabbarunselected.js
@@ -2,8 +2,7 @@ module.exports = {
     
     setColorforUnselected: function (colord, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "CDVTabBarUnselected", "setColorforUnselected", [colord]);
-    };
-    
+    },
     setBottomPadding: function (padding, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "CDVTabBarUnselected", "setBottomPadding", [padding]);
     }
